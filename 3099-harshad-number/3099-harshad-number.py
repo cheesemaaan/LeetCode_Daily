662d27204a2b51f6acdcb1a90901a46342ad1255
cheesemaaan <yoon6926@gmail.com>
2024-08-12 10:00:53 +0900

    Delete 3099-harshad-number directory

--- 3099-harshad-number/3099-harshad-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
-        number = 0
-        for num in str(x):
-            number += int(num)
-        if x % number == 0:
-            return number
-        else:
-            return -1
