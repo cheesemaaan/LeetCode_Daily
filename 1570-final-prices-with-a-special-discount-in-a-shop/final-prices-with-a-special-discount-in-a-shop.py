139170879e14b80df4903128af2fa7ba4d31ad38
cheesemaaan <yoon6926@gmail.com>
2024-08-12 12:32:13 +0900

    Time: 51 ms (44.35%) | Memory: 16.7 MB (38.23%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,21 +1,14 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         answer = []
-        check = []
+        
         for i in range(len(prices)):
+            discount  =  0
             for j in range(i+1,len(prices)):
             
                 if prices[i]>=prices[j]:
-                    check.append(prices[i]-prices[j])
-                    
-                    continue
-                    
-                else:
-                    continue
-           
-            if check == []:
-                answer.append(prices[i])
-            else:
-                answer.append(check[0])
-            check = []
+                    discount = prices[j]
+                    break
+            answer.append(prices[i]-discount)
+                  
         return answer
