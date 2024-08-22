4fd453e60980b2741a93807c28229685765a391a
cheesemaaan <yoon6926@gmail.com>
2024-08-22 09:28:53 +0900

    Time: 70 ms (59.79%) | Memory: 16.6 MB (7.62%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.py
+++ 3533-snake-in-matrix/snake-in-matrix.py
@@ -1,12 +1,37 @@
+# class Solution:
+#     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+#         answer = 0
+#         up_count = commands.count("UP")
+#         down_count = commands.count("DOWN")
+#         left_count = commands.count("LEFT")
+#         right_count = commands.count("RIGHT")
+#         answer += right_count
+#         answer -= left_count
+#         answer += down_count*n
+#         answer -= up_count*n
+
+
+#         return answer
+
+
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
         answer = 0
-        up_count = commands.count("UP")
-        down_count = commands.count("DOWN")
-        left_count = commands.count("LEFT")
-        right_count = commands.count("RIGHT")
+        up_count = down_count = left_count = right_count = 0
+
+        for command in commands:
+            if command == "UP":
+                up_count += 1
+            elif command == "DOWN":
+                down_count += 1
+            elif command == "LEFT":
+                left_count += 1
+            elif command == "RIGHT":
+                right_count += 1
+
         answer += right_count
         answer -= left_count
-        answer += down_count*n
-        answer -= up_count*n
+        answer += down_count * n
+        answer -= up_count * n
+
         return answer
