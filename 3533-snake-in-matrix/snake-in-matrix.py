0fee02db049e230a25eed9bb6b933c2bef77ed3c
cheesemaaan <yoon6926@gmail.com>
2024-08-22 09:33:15 +0900

    Time: 59 ms (97.41%) | Memory: 16.5 MB (86.86%) - LeetSync

--- 3533-snake-in-matrix/snake-in-matrix.py
+++ 3533-snake-in-matrix/snake-in-matrix.py
@@ -14,24 +14,28 @@
 #         return answer
 
 
+# class Solution:
+#     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+#         answer = 0
+#         up_count = down_count = left_count = right_count = 0
+
+#         for command in commands:
+#             if command == "UP":
+#                 up_count += 1
+#             elif command == "DOWN":
+#                 down_count += 1
+#             elif command == "LEFT":
+#                 left_count += 1
+#             elif command == "RIGHT":
+#                 right_count += 1
+
+#         answer += right_count
+#         answer -= left_count
+#         answer += down_count * n
+#         answer -= up_count * n
+
+#         return answer
+
 class Solution:
     def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
-        answer = 0
-        up_count = down_count = left_count = right_count = 0
-
-        for command in commands:
-            if command == "UP":
-                up_count += 1
-            elif command == "DOWN":
-                down_count += 1
-            elif command == "LEFT":
-                left_count += 1
-            elif command == "RIGHT":
-                right_count += 1
-
-        answer += right_count
-        answer -= left_count
-        answer += down_count * n
-        answer -= up_count * n
-
-        return answer
+        return sum(1 if cmd == "RIGHT" else -1 if cmd == "LEFT" else n if cmd == "DOWN" else -n for cmd in commands)
