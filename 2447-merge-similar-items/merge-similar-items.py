e0f86e25030d2729b28cade28f230e8da3c8f80f
cheesemaaan <yoon6926@gmail.com>
2024-08-26 09:26:56 +0900

    Time: 123 ms (7.86%) | Memory: 17.5 MB (18.14%) - LeetSync

---
+++ 2447-merge-similar-items/merge-similar-items.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mergeSimilarItems(self, items1: List[List[int]], items2: List[List[int]]) -> List[List[int]]:
+        count_dict = {}
+        for value,weight in items1:
+            
+            if value not in count_dict:
+                count_dict[value] = 0
+                count_dict[value] += weight
+            else:
+                count_dict[value] +=weight
+
+        for value,weight in items2:
+
+            if value not in count_dict:
+                count_dict[value] = 0
+                count_dict[value] += weight
+            else:
+                count_dict[value] +=weight
+        print(list(count_dict.items()))
+        return [[key, value] for key, value in sorted(count_dict.items())]
