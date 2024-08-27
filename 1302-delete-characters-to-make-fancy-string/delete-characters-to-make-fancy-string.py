c0eb409f57de0c66bc752dec51ad1e5435470e9c
cheesemaaan <yoon6926@gmail.com>
2024-08-27 09:48:38 +0900

    Time: 392 ms (54.38%) | Memory: 18.7 MB (33.27%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,9 +1,10 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-            result = []  # 빈 리스트로 시작
-            for char in s:
-        # 현재 문자를 추가할지 결정하기 위해 리스트의 길이를 확인
-                if len(result) < 2 or not (char == result[-1] == result[-2]):
-                    result.append(char)
-            return ''.join(result)
-
+            result = s[0:2]  # 초기 두 문자(또는 그 이하)를 결과에 미리 추가
+        
+            for i in range(2, len(s)):
+                # 마지막 두 문자와 현재 문자가 같지 않다면 결과에 추가
+                if not (s[i] == result[-1] == result[-2]):
+                    result += s[i]
+        
+            return result
