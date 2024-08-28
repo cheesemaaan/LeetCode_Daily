17d3b3d0eb61dad8808b911c22a7b5d0dc9b2e6d
cheesemaaan <yoon6926@gmail.com>
2024-08-28 14:40:31 +0900

    Time: 37 ms (77.96%) | Memory: 16.4 MB (88.06%) - LeetSync

--- 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
+++ 1560-number-of-students-doing-homework-at-a-given-time/number-of-students-doing-homework-at-a-given-time.py
@@ -1,7 +1,9 @@
 class Solution:
     def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
         answer = 0
-        for i,c in enumerate(zip(startTime,endTime)):
-            if startTime[i] <= queryTime and queryTime <= endTime[i]:
-                answer +=1
+        for start,end in zip(startTime,endTime):
+                if start <= queryTime <= end:
+                        answer +=1
         return answer
+
+        
