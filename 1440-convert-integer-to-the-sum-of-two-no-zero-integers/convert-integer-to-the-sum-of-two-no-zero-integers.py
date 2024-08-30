6c87b339d0addd30c75e4382a40909c24e556406
cheesemaaan <yoon6926@gmail.com>
2024-08-30 09:52:31 +0900

    Time: 34 ms (67.04%) | Memory: 16.6 MB (21.34%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(1,n):
             a=i
             b=n-i
-            if '0' not in str(a) and '0' not in str(b):
+            if '0' not in str(a)+str(b):
                 answer.append(a)
                 answer.append(b)
                 return answer
