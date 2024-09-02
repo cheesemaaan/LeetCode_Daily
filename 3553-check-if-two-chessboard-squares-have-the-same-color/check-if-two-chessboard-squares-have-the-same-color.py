07e7316f45083a09cc8c9b0458e9b10d5f7ba35c
cheesemaaan <yoon6926@gmail.com>
2024-09-02 09:39:04 +0900

    Time: 35 ms (100.00%) | Memory: 16.5 MB (100.00%) - LeetSync

--- 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/check-if-two-chessboard-squares-have-the-same-color.py
@@ -1,10 +1,7 @@
 class Solution:
     def checkTwoChessboards(self, coordinate1: str, coordinate2: str) -> bool:
-        # 첫 번째 좌표의 색을 계산
-        color1 = (ord(coordinate1[0])  + int(coordinate1[1])) % 2
-        
-        # 두 번째 좌표의 색을 계산
-        color2 = (ord(coordinate2[0])  + int(coordinate2[1])) % 2
-        
-        # 두 좌표의 색이 동일한지 확인
-        return color1 == color2
+       x_1,y_1 = coordinate1
+       x_2,y_2 = coordinate2
+       color1 = (ord(x_1)+int(y_1))%2
+       color2 = (ord(x_2)+int(y_2))%2
+       return color1 == color2
