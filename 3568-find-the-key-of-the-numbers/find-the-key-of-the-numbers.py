e8a7c451f9c581351200dd2d6cd85cfdd472d9fe
cheesemaaan <yoon6926@gmail.com>
2024-09-03 10:04:07 +0900

    Time: 42 ms (15.61%) | Memory: 16.6 MB (48.82%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -1,10 +1,10 @@
 class Solution:
     def generateKey(self, num1: int, num2: int, num3: int) -> int:
         answer =""
-        str_1 = str(num1).zfill(4)
+        str_1 = str(num1).zfill(4) 
         str_2 = str(num2).zfill(4)
         str_3 = str(num3).zfill(4)
         for c in zip(str_1,str_2,str_3):
-            answer += min(c)
-        answer = answer.lstrip('0') or '0'
+            answer += min(c)  # 각 자릿수의 문자열 모음인데도 min함수 적용가능한 점 기억하기!
+        # answer = answer.lstrip('0') or '0'
         return int(answer)
