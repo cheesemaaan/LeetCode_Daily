e2cc8c9d96b0f33ead50d61d8ad8098965480202
cheesemaaan <yoon6926@gmail.com>
2024-09-03 09:19:58 +0900

    Time: 46 ms (9.20%) | Memory: 16.6 MB (5.19%) - LeetSync

---
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+    def generateKey(self, num1: int, num2: int, num3: int) -> int:
+        answer =""
+        str_1 = str(num1).zfill(4)
+        str_2 = str(num2).zfill(4)
+        str_3 = str(num3).zfill(4)
+        print(type(str_1))
+        for c in zip(str_1,str_2,str_3):
+            answer += min(c)
+        answer = answer.lstrip('0') or '0'
+        return int(answer)
