10726fea8449b3d904958abed4d110db4ace9585
cheesemaaan <yoon6926@gmail.com>
2024-09-03 09:20:07 +0900

    Time: 33 ms (76.56%) | Memory: 16.5 MB (83.08%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -4,7 +4,6 @@ class Solution:
         str_1 = str(num1).zfill(4)
         str_2 = str(num2).zfill(4)
         str_3 = str(num3).zfill(4)
-        print(type(str_1))
         for c in zip(str_1,str_2,str_3):
             answer += min(c)
         answer = answer.lstrip('0') or '0'
