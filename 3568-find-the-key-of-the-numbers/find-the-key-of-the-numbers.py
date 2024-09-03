8ef78299184ede1255315d5cb0598201664dcda2
cheesemaaan <yoon6926@gmail.com>
2024-09-03 10:05:28 +0900

    Time: 37 ms (50.28%) | Memory: 16.4 MB (99.24%) - LeetSync

--- 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
+++ 3568-find-the-key-of-the-numbers/find-the-key-of-the-numbers.py
@@ -5,6 +5,6 @@ class Solution:
         str_2 = str(num2).zfill(4)
         str_3 = str(num3).zfill(4)
         for c in zip(str_1,str_2,str_3):
-            answer += min(c)  # 각 자릿수의 문자열 모음인데도 min함수 적용가능한 점 기억하기!
-        # answer = answer.lstrip('0') or '0'
+            answer += min(c)  # 각 자릿수의 문자열 모음인 튜플형태이므로  min함수 적용가능한 점 기억하기!
+        answer = answer.lstrip('0') or '0'
         return int(answer)
