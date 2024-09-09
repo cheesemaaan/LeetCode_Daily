38d2f587369137683477ae98ec7511f190f98f6f
cheesemaaan <yoon6926@gmail.com>
2024-09-09 09:26:12 +0900

    Time: 25 ms (96.80%) | Memory: 16.5 MB (20.13%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -1,8 +1,7 @@
 class Solution:
     def isSameAfterReversals(self, num: int) -> bool:
         # answer = True
-        if num==0:
-            return True
-        if abs(num) > 0 and int(str(num)[-1])!=0:
+      
+        if int(str(num)[-1])!=0 or num==0 :
             return True
         else: return False
