e6ad941d0502711a1229af25a2f5f0695ead532e
cheesemaaan <yoon6926@gmail.com>
2024-09-13 09:36:02 +0900

    Time: 27 ms (93.36%) | Memory: 16.4 MB (88.51%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -1,9 +1,9 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
         count_dict = {}
-        for i,c in enumerate(s):
+        for c in s:
             if c not in count_dict:
-                count_dict[c]=[i,1]
+                count_dict[c]=[1]
             else:
                 return c
         
