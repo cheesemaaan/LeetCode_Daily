73cd08f1c24a044f61aefeb1c0ce824140a6f64f
cheesemaaan <yoon6926@gmail.com>
2024-09-19 10:12:38 +0900

    Time: 55 ms (19.09%) | Memory: 16.5 MB (67.64%) - LeetSync

--- 2791-find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.py
+++ 2791-find-the-losers-of-the-circular-game/find-the-losers-of-the-circular-game.py
@@ -1,21 +1,16 @@
 class Solution:
     def circularGameLosers(self, n: int, k: int) -> List[int]:
+        cur_loc = 1
+        cnt = 0
+        visited = set([cur_loc])
+        
         while True:
-            cur_loc = 1
-            cnt = 0
-            visted =set([cur_loc])
-            answer = set()
-            while True:
-                cnt +=1
-                cur_loc = (cur_loc +(cnt*k)) %n 
-                if cur_loc ==0:
-                    cur_loc = n
-                
-                if cur_loc not in visted:
-                    visted.add(cur_loc)
-                else:
-                    break
-            for i in range(1,n+1):
-                if i not in visted:
-                    answer.add(i)
-            return list(sorted(answer))
+            cnt += 1
+            cur_loc = (cur_loc + cnt * k - 1) % n + 1  # 1-based 인덱싱 유지
+            
+            if cur_loc in visited:
+                break
+            visited.add(cur_loc)
+        
+        # 공을 받지 못한 친구들 찾기
+        return sorted([i for i in range(1, n + 1) if i not in visited])
