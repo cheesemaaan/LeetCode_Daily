0ddb429eb7a75b53d6035c6da32786b36eb4d04c
cheesemaaan <yoon6926@gmail.com>
2024-09-20 09:51:21 +0900

    Time: 53 ms (31.34%) | Memory: 16.7 MB (28.62%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.py
@@ -1,9 +1,10 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        answer =[]
-        nums_dict=Counter(nums)
-        for num,cnt in nums_dict.items():
-            if cnt >= 2:
-                answer.append(num)
+        # answer =[]
+        # nums_dict=Counter(nums)
+        # for num,cnt in nums_dict.items():
+        #     if cnt >= 2:
+        #         answer.append(num)
+        # return answer
 
-        return answer
+        return [num for num,cnt in Counter(nums).items() if cnt>1]
