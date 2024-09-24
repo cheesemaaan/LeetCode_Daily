3614d477339af6c5b9c5165f947010edc0f7a55e
cheesemaaan <yoon6926@gmail.com>
2024-09-24 09:45:08 +0900

    Time: 121 ms (5.01%) | Memory: 18.1 MB (7.63%) - LeetSync

---
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findColumnWidth(self, grid: List[List[int]]) -> List[int]:
+        answer =[]
+        for i in range(len(grid[0])):
+            max_length =0 # 같은 칼럼만 얻기 위해 print때순서반대로!
+            for j in range(len(grid)):
+                print(grid[j][i])
+                max_length = max(len(str(grid[j][i])),max_length)
+            
+            answer.append(max_length)
+        
+        return answer
+                
