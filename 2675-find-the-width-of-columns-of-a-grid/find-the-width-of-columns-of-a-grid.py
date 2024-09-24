1e1de77ed4a3f06a31b07d998b517b3932a20b34
cheesemaaan <yoon6926@gmail.com>
2024-09-24 09:45:16 +0900

    Time: 110 ms (15.47%) | Memory: 18 MB (40.31%) - LeetSync

--- 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.py
+++ 2675-find-the-width-of-columns-of-a-grid/find-the-width-of-columns-of-a-grid.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(grid[0])):
             max_length =0 # 같은 칼럼만 얻기 위해 print때순서반대로!
             for j in range(len(grid)):
-                print(grid[j][i])
                 max_length = max(len(str(grid[j][i])),max_length)
             
             answer.append(max_length)
