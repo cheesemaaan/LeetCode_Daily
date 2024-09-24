d0ce74138f60affaf78855eda2aa6e758ce90d05
cheesemaaan <yoon6926@gmail.com>
2024-09-24 10:08:21 +0900

    Time: 218 ms (60.03%) | Memory: 16.9 MB (79.59%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -3,7 +3,6 @@ class Solution:
         answer =[]
         even_nums = [x for x in nums if x%2 ==0]
         cnt = Counter(even_nums)
-        print(cnt)
         if  not even_nums:
             return -1
         max_value = max(cnt.values())
