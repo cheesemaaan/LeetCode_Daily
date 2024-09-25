42a0467f75569ebebb92f9bb290f001489ff010c
cheesemaaan <yoon6926@gmail.com>
2024-09-25 10:04:58 +0900

    Time: 46 ms (69.12%) | Memory: 16.7 MB (28.02%) - LeetSync

--- 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.py
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.py
@@ -2,7 +2,6 @@ class Solution:
     def stableMountains(self, height: List[int], threshold: int) -> List[int]:
         answer=[]
         for i in range(1,len(height)):
-            if height[i] != 0 and height[i-1]>threshold:
+            if  height[i-1]>threshold:
                 answer.append(i)
-        
         return answer
