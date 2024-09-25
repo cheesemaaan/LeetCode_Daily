8ee8ae2406f3835f248d8a93568d43ebf0550f42
cheesemaaan <yoon6926@gmail.com>
2024-09-25 09:49:49 +0900

    Time: 49 ms (25.08%) | Memory: 16.8 MB (5.07%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.py
+++ 1755-defuse-the-bomb/defuse-the-bomb.py
@@ -1,23 +1,42 @@
-class Solution:
-    def decrypt(self, code: List[int], k: int) -> List[int]:
-        sum_code = 0
-        answer=[0]*len(code)
-        if k == 0:
-            return answer
-        elif k>0:
-            for i in range(len(code)):
-                for s in range(k):
-                    sum_code+=code[((i+s+1)%len(code))]
+# class Solution:
+#     def decrypt(self, code: List[int], k: int) -> List[int]:
+#         sum_code = 0
+#         answer=[0]*len(code)
+#         if k == 0:
+#             return answer
+#         elif k>0:
+#             for i in range(len(code)):
+#                 for s in range(k):
+#                     sum_code+=code[((i+s+1)%len(code))]
                     
                 
-                answer[i]=sum_code
-                sum_code=0
-        elif k < 0:
-             for i in range(len(code)):
-                for s in range(-k):
-                    sum_code+=code[((i-s-1)%len(code))]
-                answer[i]=sum_code
-                sum_code=0
+#                 answer[i]=sum_code
+#                 sum_code=0
+#         elif k < 0:
+#              for i in range(len(code)):
+#                 for s in range(-k):
+#                     sum_code+=code[((i-s-1)%len(code))]
+#                 answer[i]=sum_code
+#                 sum_code=0
+        
+#         return answer
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        answer = [0] * n
+        
+        if k == 0:
+            return answer
+        
+        for i in range(n):
+            sum_code = 0
+            if k > 0:
+                for j in range(1, k + 1):
+                    sum_code += code[(i + j) % n]
+            else:
+                for j in range(1, -k + 1):
+                    sum_code += code[(i - j) % n]
+            answer[i] = sum_code
         
         return answer
-                
