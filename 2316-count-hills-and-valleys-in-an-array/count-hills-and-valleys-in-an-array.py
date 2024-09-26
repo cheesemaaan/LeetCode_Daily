5037c06d66c872239e32af53cc86eb8191af4efb
cheesemaaan <yoon6926@gmail.com>
2024-09-26 09:46:16 +0900

    Time: 45 ms (22.52%) | Memory: 16.4 MB (69.31%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -1,13 +1,14 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        not_dulplicated_nums =[]
+        # not_dulplicated_nums =[]
         answer=0
-        for i in range(len(nums)):
-            if i==0:
-                not_dulplicated_nums.append(nums[i])
-            else:
-                if nums[i] !=nums[i-1]:
-                    not_dulplicated_nums.append(nums[i])
+        # for i in range(len(nums)):
+        #     if i==0:
+        #         not_dulplicated_nums.append(nums[i])
+        #     else:
+        #         if nums[i] !=nums[i-1]:
+        #             not_dulplicated_nums.append(nums[i])
+        not_dulplicated_nums = [k for k, _ in groupby(nums)]
         
         for i in range(1,len(not_dulplicated_nums)-1):
             if not_dulplicated_nums[i] >not_dulplicated_nums[i-1] and not_dulplicated_nums[i]>not_dulplicated_nums[i+1]:
