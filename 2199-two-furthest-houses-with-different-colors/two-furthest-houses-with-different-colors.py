5eb8e4255b448c51997b7dc13dd25e561b1ef621
cheesemaaan <yoon6926@gmail.com>
2024-09-27 09:24:04 +0900

    Time: 48 ms (30.15%) | Memory: 16.4 MB (71.37%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.py
@@ -5,7 +5,6 @@ class Solution:
         for i in range(n): 
             for j in range(i+1,n):
                 if colors[i] != colors[j] and abs(i-j) > max_distance:
-                    max_distance = max(max_distance,abs(i-j))
-                    
+                    max_distance = abs(i-j)
         return max_distance
 
