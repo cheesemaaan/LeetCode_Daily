8ae9f147609aa84a17725fb48aa409f9a8eda322
cheesemaaan <yoon6926@gmail.com>
2024-10-04 09:36:10 +0900

    Time: 72 ms (54.16%) | Memory: 16.8 MB (8.79%) - LeetSync

---
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float: 
+       answer = 0
+       prev = 0
+       for upper,percent in brackets:
+            if income >= upper:
+                amount = upper - prev
+                answer += amount*(percent*0.01)
+                prev = upper
+                print(answer)
+            else:
+                amount = income -prev
+                answer += amount*(percent*0.01)
+                print(answer)
+                break
+
+       return answer
