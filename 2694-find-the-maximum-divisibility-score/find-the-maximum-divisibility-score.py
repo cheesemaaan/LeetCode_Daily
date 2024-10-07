ea940e88734217eaa667b8df8fb91408f7495017
cheesemaaan <yoon6926@gmail.com>
2024-10-07 09:58:17 +0900

    Time: 2560 ms (54.15%) | Memory: 17 MB (23.90%) - LeetSync

--- 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.py
+++ 2694-find-the-maximum-divisibility-score/find-the-maximum-divisibility-score.py
@@ -7,10 +7,14 @@ class Solution:
                 if num%divisor ==0:
                     score +=1
             d[divisor] = score
+        # 점수는 내림차순, divisor는 오름차순으로 정렬
+        sorted_divisors = sorted(d.items(), key=lambda x: (-x[1], x[0]))
         
+        # 첫 번째 항목의 divisor 반환
+        return sorted_divisors[0][0]
      
-        score_max = max(d.values())
-        return min(divisor for divisor,score in d.items() if score==score_max)
+        # score_max = max(d.values())
+        # return min(divisor for divisor,score in d.items() if score==score_max)
 
         
     
