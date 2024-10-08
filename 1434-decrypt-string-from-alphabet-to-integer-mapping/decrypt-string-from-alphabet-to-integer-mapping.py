471631a2d6c525cd341f414bcaba503a750af95c
cheesemaaan <yoon6926@gmail.com>
2024-10-08 09:25:42 +0900

    Time: 38 ms (38.48%) | Memory: 16.6 MB (30.35%) - LeetSync

--- 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -2,17 +2,12 @@ class Solution:
     def freqAlphabets(self, s: str) -> str:
         i=0
         answer =""
-        print(s[2])
         while i<len(s):
             if i+2 < len(s) and s[i+2] == "#":
                 answer += chr(int(s[i:i+2])+96)
-                print(answer)
-                
                 i += 3
             else:
                 answer +=chr(int(s[i])+96)
-                print(answer)
-                
                 i+=1
         return answer
 
