9728cfcc5dab88421ad4de66cd63382cbb927866
cheesemaaan <yoon6926@gmail.com>
2024-10-08 09:58:36 +0900

    Time: 51 ms (95.78%) | Memory: 16.5 MB (42.58%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.py
@@ -1,9 +1,14 @@
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
-        answer=0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if abs(nums[i]-nums[j]) == k:
-                    answer +=1
-
-        return answer
+        freq = defaultdict(int)
+        count = 0
+        
+        for num in nums:
+            # num - k나 num + k가 이미 존재하는 경우 쌍을 형성할 수 있음
+            count += freq[num - k]
+            count += freq[num + k]
+            
+            # 현재 숫자의 빈도를 증가
+            freq[num] += 1
+        
+        return count
